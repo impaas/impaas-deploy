88dc5d2e334615c3ab409b999af04e5f9c066af0
aaryan-dharmadhikari <56603891+aaryan-dharmadhikari@users.noreply.github.com>
2024-02-22 15:34:10 +0000

    bumped to v1.8

--- dist/index.cjs
+++ dist/index.cjs
@@ -37527,7 +37527,8 @@ async function deployToImPaaS(appName, deploymentToken) {
   try {
     const apiUrl = `http://impaas.uk/apps/${appName}/deploy`;
     const imageUrl = `ghcr.io/${github.context.repo.owner}/${github.context.repo.repo}:latest`;
-    
+    // const imageUrl = `ghcr.io/aaryan-dharmadhikari/aaryan-dharmadhikari/flask-demo-app:latest`;
+    core.debug(`Deploying ${imageUrl} to ${apiUrl} with token ${deploymentToken}`);
     const response = await axios.post(apiUrl, { image: imageUrl }, {
       headers: {
         Authorization: `Bearer ${deploymentToken}`
@@ -37545,6 +37546,8 @@ async function deployToImPaaS(appName, deploymentToken) {
   try {
     const appName = core.getInput('app-name');
     const deploymentToken = core.getInput('deployment-token');
+    // const appName = 'flask-demo-app';
+    // const deploymentToken = 'f5a768e7a09100f8d28eec7ba42d15d745ff3acd8a222b731a3dcd2c07f044b2';
 
     await deployToImPaaS(appName, deploymentToken);
   } catch (error) {
